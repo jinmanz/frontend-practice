f609488a38f4354c4a06621185ad1514fc47e252
jinmanz <zjm@uws110324usr.uws.ualberta.ca>
2016-05-26 15:53:23 -0600

    o'clock to show and hide collapse

--- responsive/case1/case1.js
+++ responsive/case1/case1.js
@@ -16,12 +16,16 @@ function showNav(){
 	var collapse = document.getElementById("nav-collapse");
 	button.onclick=(function(){
 		
-		collapse.style.display="block";
+		if(collapse.style.display=="none"){
+			collapse.style.display="block";
+		}else{
+			
+			collapse.style.display="none";
+		}
+		
 		return false;//如果你想用JS代码来局部改变一些数据而不引起页面其他部位的变化，那么你就应该在该onclick事件代码的后面加上return false；
 	});
-	collapse.onmouseleave=(function(){
-		collapse.style.display="none";
-	});
+	
 
 }
 
